5385e95ed186320ed5477a7eb2fb8cf15f31bacb
安井大晟 <taisei_yasui@yasui-taiseiMacBook-Air.local>
2023-08-05 17:28:43 +0900

    create user

--- util/random.go
+++ util/random.go
@@ -5,8 +5,6 @@ import (
 	"math/rand"
 	"strings"
 	"time"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -47,13 +45,4 @@ func RandomCurrency() string {
 
 func RandomEmail() string {
 	return fmt.Sprintf("%s@mail.com", RandomString(6))
-}
-
-// HashPassword returns the bcrypt hash of the password
-func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return "", fmt.Errorf("failed to hash password: %w", err)
-	}
-	return string(hashedPassword), nil
 }
