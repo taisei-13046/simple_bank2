61a69f7a7b09ccc47d40413a6606bd73fd56ed4c
安井大晟 <taisei_yasui@yasui-taiseiMacBook-Air.local>
2023-09-23 23:54:23 +0900

    grpc logger

--- main.go
+++ main.go
@@ -72,7 +72,8 @@ func runGrpcServer(config util.Config, store db.Store) {
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
-	grpcServer := grpc.NewServer()
+	grpcLogger := grpc.UnaryInterceptor(gapi.GrpcLogger)
+	grpcServer := grpc.NewServer(grpcLogger)
 	pb.RegisterSimpleBankServer(grpcServer, server)
 	reflection.Register(grpcServer)
 
